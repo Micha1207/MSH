e31386dd784c300d9a073942247b83c7b3be0cd8
Micha1207 <mlusz1601@gmail.com>
2025-12-23 09:30:18 -0500

    Add header file for cat command

---
+++ include/cat.h
@@ -0,0 +1,20 @@
+/*
+ * cat.h
+ * Header file for src/cat.c
+ *
+ * Author:  Micha1207
+ * Project: MSH (https://github.com/Micha1207/MSH)
+ * Date:    Dec 23, 2025
+ * License: GNU GPL v3 (full license in LICENSE file)
+ * This program comes with NO WARRANTY; to the extent permitted by law.
+ */
+#ifndef CAT_H
+#define CAT_H
+
+// Include necessary libs
+#include <stdio.h>
+
+// Function prototype (full funciton in src/cat.c)
+int cmd_cat(const char *file); 
+
+#endif
