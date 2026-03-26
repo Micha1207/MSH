b7b4a477178ab7a89364f76dde9b66f04b294f6a
Micha1207 <mlusz1601@gmail.com>
2026-03-26 16:28:00 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,8 +3,8 @@
  * MSH - Micha1207's Shell program.
  *
  * Author:  Micha1207
- * Project: MTP (https://github.com/Micha1207/MTP)
- * Date:    Mar 9, 2026
+ * Project: MSH (https://github.com/Micha1207/MSH)
+ * Date:    Mar 26, 2026
  * License: GNU GPL v3 (full license in LICENSE file)
  * This program comes with NO WARRANTY; to the extent permitted by law.
  */
