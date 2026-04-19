833b6227fc184424daee7afbd07efe38f6d2bc76
Micha1207 <mlusz1601@gmail.com>
2026-04-19 16:25:49 +0200

    Update parser.c

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
  *
  * Author:  Micha1207
  * Project: MSH (https://github.com/Micha1207/MSH)
- * Date:    Mar 9, 2026
+ * Date:    Apr 19, 2026
  * License: GNU GPL v3 (full license in LICENSE file)
  * This program comes with NO WARRANTY; to the extent permitted by law.
  */
@@ -39,7 +39,7 @@
  */
 int runcmd(const char *cmd){
   char cmd_copy[512], *arg[16];
-  int i = 0;
+  int i = 0, ext_st = 0;
   strncpy(cmd_copy, cmd, sizeof(cmd_copy));
   cmd_copy[sizeof(cmd_copy) - 1] = '\0';
   char *token = strtok(cmd_copy, " ");
@@ -54,31 +54,31 @@ int runcmd(const char *cmd){
 
   if (!arg[0]){ return 0; } else {
       if (strcmp (arg[0], "cat") == 0 && arg[1]){
-	cmd_cat(arg[1]);
+	ext_st = cmd_cat(arg[1]);
       } else if (strcmp (arg[0], "cd") == 0 && arg[1]){
-	cmd_cd(arg[1]);
+	ext_st = cmd_cd(arg[1]);
       } else if (strcmp (arg[0], "cls") == 0 || strcmp (arg[0], "clear") == 0){
-	cmd_clear();
+	ext_st = cmd_clear();
       } else if (strcmp (arg[0], "echo") == 0 && arg[1]){
-	cmd_echo(arg);
+	ext_st = cmd_echo(arg);
       } else if (strcmp (arg[0], "ls") == 0 || strcmp (arg[0], "dir") == 0){
-	cmd_ls(arg[1]);
+	ext_st = cmd_ls(arg[1]);
       } else if (strcmp (arg[0], "mkdir") == 0 && arg[1]){
-	cmd_mkdir(arg[1]);
+	ext_st = cmd_mkdir(arg[1]);
       } else if (strcmp (arg[0], "touch") == 0 && arg[1]){
-	cmd_touch(arg[1]);
+	ext_st = cmd_touch(arg[1]);
       } else if (strcmp (arg[0], "pwd") == 0){
-	cmd_pwd();
+	ext_st = cmd_pwd();
       } else if (strcmp (arg[0], "rm") == 0 && arg[1]){
-	cmd_rm(arg[1]);
+	ext_st = cmd_rm(arg[1]);
       } else if (strcmp (arg[0], "help") == 0){
-	cmd_help();
+	ext_st = cmd_help();
       } else if (strcmp (arg[0], "run") == 0){
-	cmd_run(&arg[1]);
+	ext_st = cmd_run(&arg[1]);
       } else {
-	cmd_run(arg);
+	ext_st = cmd_run(arg);
       }
     }
 
-  return 0;
+  return ext_st;
 }
