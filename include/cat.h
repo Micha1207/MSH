658fff463a0153b69d953eb58d9b41cdd5509003
Micha1207 <mlusz1601@gmail.com>
2026-06-04 14:11:35 +0200

    Create cat.h

---
+++ include/cat.h
@@ -0,0 +1,20 @@
+/*
+ * cat.h
+ * MSH - Micha1207's Shell program.
+ *
+ * This is a header file for src/cat.c
+ *
+ * Author:  Micha1207
+ * Project: MSH (https://github.com/Micha1207/MSH)
+ * Date:    Mar 11, 2026
+ * License: GNU GPL v3 (full license in LICENSE file)
+ * This program comes with NO WARRANTY; to the extent permitted by law.
+ */
+#ifndef CAT_H
+#define CAT_H
+
+#include <stdio.h>
+
+int cmd_cat(const char *file);
+
+#endif
